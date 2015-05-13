08fa785cd209d644d2b9960b66c01ee61ed0e14b
DavideDaniel <davidedaniel@gmail.com>
2015-05-12 22:09:43 -0400

    added Makefile for mocha test

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-
-
+// dependencies
 var fs = require("fs");
 
-fs.readFile("server.js", function (err, data) {
-    if (data) {
-        var text = data.toString();
-        var commentArray = text.match(new RegExp(/\/\/...*\b/i));
-        var counter = 0;
-        for (var i = 0; i < commentArray.length; i++) {
-            commentArray[i].replace(/\/\//, '')
-            if (commentArray[i].indexOf('//') < 0) {
-                counter++
-            }
-        }
-        if (counter === commentArray.length) {
-            var longString = ''
-            for (var i = 0; i < commentArray.length; i++) {
-                longString += commentArray[i] + '\n'
-            };
-            fs.writeFile("README.md", longString, function (err) {
-                if (err) {
-                    console.error(err)
-                };
-            })
-            fs.readFile("README.md", function (err, data) {
-                if (data) {
-                    var text = data.toString();
-                    if (text === longString) {
-                        return true
+// module for commentDoc
+
+module.exports = {
+    extract: function (fileToRead, fileToWrite) {
+
+        fs.readFile(fileToRead, function (err, data) {
+            if (data) {
+                var text = data.toString();
+                var commentArray = text.match(new RegExp(/\/\/...*\b/i));
+                var counter = 0;
+                for (var i = 0; i < commentArray.length; i++) {
+                    commentArray[i].replace(/\/\//, '')
+                    if (commentArray[i].indexOf('//') < 0) {
+                        counter++
                     }
                 }
-            })
-        }
-    }
+                if (counter === commentArray.length) {
+                    var longString = ''
+                    for (var i = 0; i < commentArray.length; i++) {
+                        longString += commentArray[i] + '\n'
+                    };
+                    fs.writeFile(fileToWrite, longString, function (err) {
+                        if (err) {
+                            console.error(err)
+                        };
+                    })
+                    fs.readFile(fileToWrite, function (err, data) {
+                        if (data) {
+                            var text = data.toString();
+                            if (text === longString) {
+                                console.log(fileToWrite + ' written succesfully.');
+                            }
+                        }
+                    })
+                }
+            }
 
-});
+        });
+    };
+
+}
